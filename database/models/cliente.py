db78bdf1d859b8111523dd0d5bb22cd6cdfa9225
Luís Guilherme Josepette Marchetto <91769780+guiijoo@users.noreply.github.com>
2024-04-13 16:32:33 -0300

    config.py

--- database/models/cliente.py
+++ database/models/cliente.py
@@ -1,10 +1,11 @@
-from peewee import CharField, DateField, Model
+from peewee import CharField, DateTimeField, Model
 from database.database import db
+import datetime
 
 class Cliente(Model):
     name = CharField()
     email = CharField()
-    data_registro = DateField()
+    data_registro = DateTimeField(default=datetime.datetime.now)
     
     class Meta:
         database = db
